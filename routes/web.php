2fd6fb2aaf22c0ba7fbb40f998fe6277a1cabdca
HakuryuSato <r700540@gmail.com>
2024-08-28 22:25:07 +0900

    ルーティング追加

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/create_edit_article', function () {
+    return view('create_edit_article');
+});
