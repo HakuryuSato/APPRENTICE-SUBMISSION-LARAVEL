95b0dc910a78cf0a3020521143bc58fda3d33c77
HakuryuSato <r700540@gmail.com>
2024-08-30 20:17:54 +0900

    ログイン途中

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,50 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User; // ユーザーモデルをインポート
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function showRegistrationForm()
+    {
+        return view('auth.register');
+    }
+
+    public function login(Request $request)
+    {
+        // ログインロジックをここに記述
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'user_name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->route('register')
+                             ->withErrors($validator)
+                             ->withInput();
+        }
+
+        $user = User::create([
+            'user_name' => $request->user_name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        Auth::login($user);
+
+        return redirect()->route('home'); // ホームページへのリダイレクト
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -9,26 +9,32 @@ use App\Http\Controllers\ArticleController;
 // ホーム
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-// 認証関連
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
-Route::post('/register', [AuthController::class, 'register']);
-
-// 編集ページ
-Route::get('/editor', [ArticleController::class, 'create'])->middleware('auth')->name('editor.create');
-Route::get('/editor/{slug}', [ArticleController::class, 'edit'])->middleware('auth')->name('editor.edit');
-
-// 記事ページ
+// 記事
 route::get('/article/{slug}', [ArticleController::class, 'show'])->name('article.show');
 Route::post('/article', [ArticleController::class, 'store'])->middleware('auth')->name('article.store');
 Route::put('/article/{slug}', [ArticleController::class, 'update'])->middleware('auth')->name('article.update');
 Route::delete('/article/{slug}', [ArticleController::class, 'destroy'])->middleware('auth')->name('article.delete');
 
+// 著者
+Route::get('/profile/{username}', [ProfileController::class, 'show'])->name('profile.show');
+Route::get('/profile/{username}/favorites', [ProfileController::class, 'favorites'])->name('profile.favorites');
+
+
+// ログイン
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+// 登録
+Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+
+// 記事　作成 編集
+Route::get('/editor', [ArticleController::class, 'create'])->middleware('auth')->name('editor.create');
+Route::get('/editor/{slug}', [ArticleController::class, 'edit'])->middleware('auth')->name('editor.edit');
+
+
 // Comments
 // Route::post('/article/{slug}/comment', [CommentController::class, 'store'])->middleware('auth')->name('comment.store');
 // Route::delete('/article/{slug}/comment/{id}', [CommentController::class, 'destroy'])->middleware('auth')->name('comment.delete');
 
-// Profile pages
-Route::get('/profile/{username}', [ProfileController::class, 'show'])->name('profile.show');
-Route::get('/profile/{username}/favorites', [ProfileController::class, 'favorites'])->name('profile.favorites');
+
